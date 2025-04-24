e15a8309838503b962d6b11dce6574525e9b3f95
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-04-24 22:31:33 +0500

    some features added and code refactor

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -17,31 +17,39 @@ enum Move: String, CaseIterable, Identifiable {
 }
 
 //Rule of Game
-enum Target {
+enum Target: String, CaseIterable {
     case win, lose
 }
 
 struct ContentView: View {
-    @State var computerAnswer = Move.allCases.randomElement()
+    @State var computerAnswer = Move.allCases.randomElement()!
+    @State var target = Target.allCases.randomElement()!
+    @State var score: Int = 0
     
     var body: some View {
         ZStack {
             VStack {
+                //Text to show player to win or lose
+                StyledText(text: target == .win ? "Try To Beat Me" : "I'm Winning Now", textSize: .title2, textColor: Color("secondaryUserColor"), textWeight: .semibold)
+                    .background(BackgroundRoundedRectangle(cornerRadius: 10, minwidth: 180, maxWidth: 200, minHeight: 40, maxHeight: 40))
+                
+                //Image to show the random chose of computer
                 BackgroundRoundedRectangle(cornerRadius: 30, minwidth: 180, maxWidth: 200, minHeight: 180, maxHeight: 200)
-                    .overlay(StyledImage(image: Image(computerAnswer?.rawValue ?? Move.rock.rawValue), width: 170, height: 170, cornerRadius: 20))
+                    .overlay(StyledImage(image: Image(computerAnswer.rawValue), width: 170, height: 170, cornerRadius: 20))
+                
+                //Section to show the score
+                StyledText(text: "\(score)", textSize: .title2, textColor: Color("secondaryUserColor"), textWeight: .semibold)
+                    .background(BackgroundRoundedRectangle(cornerRadius: 10, minwidth: 80, maxWidth: 100, minHeight: 40, maxHeight: 40))
                 
                 Spacer()
                 
+                //Section to show buttons that user chooses
                 HStack() {
-                    ForEach(Move.allCases, id: \.self) { move in
+                    ForEach(Move.allCases) { move in
                         Spacer()
                         
                         Button {
-                            let playerAnswer = move
-                            
-                            startGame(playerAnswer, computerAnswer ?? Move.rock)
-                            
-                            computerAnswer = Move.allCases.randomElement()
+                            playRound(player: move)
                         } label: {
                             StyledImage(image: move.image, width: 100, height: 100, cornerRadius: 20)
                         }
@@ -53,7 +61,17 @@ struct ContentView: View {
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color("background"))
+        .background(Color("secondaryUserColor"))
+    }
+    //Starting new round after tapping a button
+    func playRound(player: Move) {
+        let currentTarget = target
+        let currentComputer = computerAnswer
+        
+        startGame(player, currentComputer, currentTarget, &score)
+        
+        target = Target.allCases.randomElement( )!
+        computerAnswer = Move.allCases.randomElement()!
     }
 }
 
@@ -62,13 +80,16 @@ struct ContentView: View {
 }
 
 //Game Logic
-func startGame(_ player: Move, _ computer: Move) {
-    switch(player, computer) {
-    case let (player, computer) where player == computer: print("draw")
-    case (.rock, .scissors),
-        (.scissors, .paper),
-        (.paper, .rock): print("player wins")
-    default : print("computer wins")
+func startGame(_ player: Move, _ computer: Move, _ target: Target, _ score: inout Int) {
+    
+    switch(player, computer, target) {
+    case (.paper, .rock, .win),
+        (.scissors, .paper, .win),
+        (.rock, .scissors, .win),
+        (.scissors, .rock, .lose),
+        (.rock, .paper, .lose),
+        (.paper, .scissors, .lose): score += 1
+    default: score += 0
     }
 }
 
@@ -99,5 +120,20 @@ struct StyledImage: View {
             .resizable()
             .frame(width: width, height: height)
             .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+    }
+}
+
+struct StyledText: View {
+    let text: String
+    let textSize: Font
+    let textColor: Color
+    let textWeight: Font.Weight
+    
+    var body: some View {
+        Text(text)
+            .font(textSize)
+            .foregroundColor(textColor)
+            .fontWeight(textWeight)
+            .padding()
     }
 }
